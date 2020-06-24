368b56e49b5babe94de41dbba0b3b4aab3d33b24
AnthonyW <anthonylavel@gmail.com>
2020-06-24 09:57:26 -0500

    Updating Aspect Oriented Programming logging property in the application files.

--- src/main/java/com/jrp/cra/logging/ApplicationLoggerAspect.java
+++ src/main/java/com/jrp/cra/logging/ApplicationLoggerAspect.java
@@ -1,11 +1,14 @@
 package com.jrp.cra.logging;
 
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
 
 @Aspect
 @Component
@@ -13,14 +16,34 @@ public class ApplicationLoggerAspect {
 
     private final Logger log = LoggerFactory.getLogger(this.getClass());
 
-    @Pointcut("within(com.jrp.cra.controller..*)")
+    @Pointcut("within(com.jrp.cra.controller..*)"+"|| within(com.jrp.cra.dao..*)")
     public void definePackagePointcuts(){
         // empty method just to name the location specified in the pointcut
     }
 
-    @Before("definePackagePointcuts()")
-    public void log(){
-        log.debug(" ----------------------- ");
+    @Around("definePackagePointcuts()")
+    public Object logAround(ProceedingJoinPoint jp){
+        log.debug(" \n \n \n ");
+        log.debug("***************** Before Method Execution ************* \n {}.{} () " +
+                "with arguments [s] = {}", jp.getSignature().getDeclaringTypeName(),
+                jp.getSignature().getName(), Arrays.toString(jp.getArgs()));
+        log.debug("___________________ \n \n \n");
+
+        Object o = null;
+
+        try {
+
+        } catch (Throwable e) {
+            // TODD Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        log.debug("***************** After Method Execution ************* \n {}.{} () " +
+                        "with arguments [s] = {}", jp.getSignature().getDeclaringTypeName(),
+                jp.getSignature().getName(), Arrays.toString(jp.getArgs()));
+        log.debug("___________________ \n \n \n");
+
+        return o;
     }
 
 }
